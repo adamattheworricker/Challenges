f753acffac6a33879ac9060737c0953e06dbf8ca
Adam Worricker <40427309+adamattheworricker@users.noreply.github.com>
2024-02-13 11:53:15 -0500

    Create C1.py

---
+++ C1.py
@@ -0,0 +1,7 @@
+name = "Adam"
+age = "35"
+like = "writing short scripts"
+
+print("My name is " + name + " and I'm " + age + " years old, and I enjoy " + like)
+
+print(f"My name is {name} and I'm {age} years old, and I enjoy{like}")
