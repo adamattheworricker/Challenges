b4de2982713e7410ff1785445a45e9efe7b9e1f9
Adam Worricker <40427309+adamattheworricker@users.noreply.github.com>
2024-02-13 11:53:27 -0500

    Create C1.cs

---
+++ C1.cs
@@ -0,0 +1,12 @@
+using System.Net.Sockets;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "adam";
+        int age = 35;        
+	   string like = "writing short scripts";
+	
+        Console.WriteLine("My name is " + name + " and I am " + age + " years old and I enjoy " + like);
+    }
+}
