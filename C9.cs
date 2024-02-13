5fb8011e65a6324da3257f3cc9d28a313aca52f1
Adam Worricker <40427309+adamattheworricker@users.noreply.github.com>
2024-02-13 12:00:03 -0500

    Create C9.cs

---
+++ C9.cs
@@ -0,0 +1,11 @@
+using System.Net.Sockets;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for(int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
