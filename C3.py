5cfd98e9248a0b052cb1e6cb342bb6caa2461752
Adam Worricker <40427309+adamattheworricker@users.noreply.github.com>
2024-02-13 11:55:20 -0500

    Create C3.py

---
+++ C3.py
@@ -0,0 +1,8 @@
+name1 = "David"
+age1 = 12
+name2 = "Cindy"
+age2 = 38
+
+combinedage= age1 + age2
+
+print("This is " + name1 + " and his mom, " + name2 + " they have a combined age of " + str(combinedage))
