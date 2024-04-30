b05a679f44434812486f6a2b313ba8be11dd3e08
adamattheworricker <adam.worricker@gmail.com>
2024-04-30 10:51:55 -0400

    added userAttempts counter to track how many guesses it takes the user to solve

--- Python/C20.py
+++ Python/C20.py
@@ -1,12 +1,16 @@
 import random
 computerNumber = random.randint(1,100)
 userNumber = 0
+userAttempts = 0
 while True:
     userNumber = int(input("Please select  number between 1 and 100\n"))
     if (userNumber > computerNumber):
+        userAttempts += 1
         print("Wrong, guess lower!")
     elif (userNumber < computerNumber):
+        userAttempts += 1
         print("Wrong, guess higher!")
     elif (userNumber == computerNumber):
-        print("You win, you guessed correctly! The computer's number was %i" %computerNumber)
+        userAttempts += 1
+        print("You win, you guessed correctly in %i attempts! The computer's number was %s" %(userAttempts,computerNumber))
         break
